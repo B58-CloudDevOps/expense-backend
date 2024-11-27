d10dd520929fba5d12c815e997af018e425aea95
CodingManoj <CodingManoj@gmail.com>
2024-11-26 19:55:44 -0500

    Making Image

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/transaction',(req,res)=>{
     }
 });
 
-//DELETE ALL TRANSACTIONS
+//DELETE ALL TRANSACTIONs
 app.delete('/transaction',(req,res)=>{
     try{
         transactionService.deleteAllTransactions(function(result){
